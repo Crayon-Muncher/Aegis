b09b478342f9e076bb65eeb35ef7e8ae11fd88be
Cody Roberts <CARGY9@mst.edu>
2017-12-16 20:00:53 -0600

    Functioning socket.io server

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const Grid = require('./grid')
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
 
-const grid = new Grid()
+app.get('/',function(req,res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server Started");
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection',function(
+	socket){
+	console.log('socket connection');
+});
