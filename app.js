64ec318e90bef346307536ba82cf9dc36b76f480
Cody Roberts <CARGY9@mst.edu>
2017-12-16 22:31:49 -0600

    Functioning players on screen capable of WASD movement

--- app.js
+++ app.js
@@ -11,8 +11,79 @@ app.use('/client',express.static(__dirname + '/client'));
 serv.listen(2000);
 console.log("Server Started");
 
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+
+	var self = {
+		x:250,
+		y:250,
+		id:id,
+		num: "" + Math.floor(10 * Math.random()),
+		pressingUp: false,
+		pressingDown: false,
+		pressingLeft: false,
+		pressingRight: false,
+		maxSpd: 10
+	}
+	self.updatePosition = function(){
+		if(self.pressingUp)
+			self.y -= self.maxSpd;
+		if(self.pressingDown)
+			self.y += self.maxSpd;
+		if(self.pressingLeft)
+			self.x -= self.maxSpd;
+		if(self.pressingRight)
+			self.x += self.maxSpd;
+	}
+	return self;
+}
+
 var io = require('socket.io')(serv,{});
-io.sockets.on('connection',function(
-	socket){
-	console.log('socket connection');
+io.sockets.on('connection',function(socket){
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+
+	SOCKET_LIST[socket.id] = socket;
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
+
+	console.log('Player Connected');
+
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+	});
+	
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		else if(data.inputId === 'down')
+			player.pressingDown = data.state;
+		else if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		else if(data.inputId === 'right')
+			player.pressingRight = data.state;
+	});
 });
+
+setInterval(function(){
+	var pack = [];
+	for(var i in PLAYER_LIST){
+		var player = PLAYER_LIST[i];
+		player.updatePosition();
+		pack.push({
+			x:player.x,
+			y:player.y,
+			num:player.num
+		});
+
+	}
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPosition', pack);
+	}
+
+},1000/25);
